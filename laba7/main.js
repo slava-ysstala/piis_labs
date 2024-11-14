abcef7fbea2bb968bc263bb3b19a1992676b61b7
Astashko Vladislava <slava.ystala@gmail.com>
2024-11-14 11:45:41 +0300

    Update main.js

--- laba7/main.js
+++ laba7/main.js
@@ -1,6 +1,8 @@
 const svgCanvas = document.getElementById('svgCanvas');
 let isDrawing = false;
 let startX, startY;
+let figure = null;
+let painting = null;
 
 svgCanvas.addEventListener('mousedown', (e) => {
     isDrawing = true;
@@ -12,24 +14,29 @@ svgCanvas.addEventListener('mousemove', (e) => {
     if (!isDrawing) return;
 
     const shapeType = document.querySelector('input[name="shape"]:checked').value;
-    svgCanvas.innerHTML = '';
 
     if (shapeType === 'circle') {
         const radius = Math.sqrt(Math.pow(e.offsetX - startX, 2) + Math.pow(e.offsetY - startY, 2));
-        const circle = `<circle cx="${startX}" cy="${startY}" r="${radius}" fill="none" stroke="black"/>`;
-        svgCanvas.innerHTML = circle;
+        figure = `<circle cx="${startX}" cy="${startY}" r="${radius}" fill="none" stroke="black"/>`;
     } else {
         const width = e.offsetX - startX;
         const height = e.offsetY - startY;
-        const rect = `<rect x="${startX}" y="${startY}" width="${width}" height="${height}" fill="none" stroke="black"/>`;
-        svgCanvas.innerHTML = rect;
+        const rectX = (width < 0) ? e.offsetX : startX;
+        const rectY = (height < 0) ? e.offsetY : startY;
+        const rectWidth = Math.abs(width);
+        const rectHeight = Math.abs(height);
+        figure = `<rect x="${rectX}" y="${rectY}" width="${rectWidth}" height="${rectHeight}" fill="none" stroke="black"/>`;
     }
+    svgCanvas.innerHTML = painting + figure;
+
 });
 
 svgCanvas.addEventListener('mouseup', () => {
     isDrawing = false;
+    painting += figure;
 });
 
 svgCanvas.addEventListener('mouseleave', () => {
     isDrawing = false;
+    painting += figure;
 });
